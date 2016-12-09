1d81b3189883ba3bbf35c866745ee5ab63bd3120
Shadow-lc <1097120884@qq.com>
2016-12-09 16:00:53 +0800

    添加了标题栏相关代码和图片

---
+++ js/title.js
@@ -0,0 +1,24 @@
+function onMouseOverBlog(){
+    document.getElementById("blog").className="navover";
+}
+function onMouseOutBlog(){
+document.getElementById("blog").className="nav";
+}
+function onMouseOverLog(){
+    document.getElementById("log").className="navover";
+}
+function onMouseOutLog(){
+document.getElementById("log").className="nav";
+}
+function onMouseOverFlower(){
+    document.getElementById("flower").className="navover";
+}
+function onMouseOutFlower(){
+document.getElementById("flower").className="nav";
+}
+function onMouseOverAbout(){
+    document.getElementById("about").className="navover";
+}
+function onMouseOutAbout(){
+document.getElementById("about").className="nav";
+}
